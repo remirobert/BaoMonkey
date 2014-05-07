83f2fda31aa8e9555f3ab52455935f8ef5c01871
iMac0de <jeremy.peltier@epitech.eu>
2014-05-07 17:09:53 +0200

    Add walking monkey animation

--- iOS/BaoMonkey/Monkey.m
+++ iOS/BaoMonkey/Monkey.m
@@ -16,11 +16,13 @@
 -(id)initWithPosition:(CGPoint)position {
     self = [super init];
     if (self) {
-        // Init the sprite of the Monkey
+        // Init the sprites of the Monkey
         sprite = [SKSpriteNode spriteNodeWithImageNamed:kSpriteImageName];
         sprite.position = position;
         
-        // Listen if the gesture is tapped
+        [self loadWalkingSprites];
+
+        // Init the notification for dropping the weapon
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(launchWeapon) name:NOTIFICATION_DROP_MONKEY_ITEM object:nil];
     }
     return self;
@@ -28,8 +30,8 @@
 
 #pragma mark - Update Sprite Functions
 
--(void)updateMonkeyPosition:(float)acceleration
-{
+-(void)updateMonkeyPosition:(float)acceleration {
+    float multiplierForDirection = 0;
     float maxX = [UIScreen mainScreen].bounds.size.width + (sprite.size.width / 2);
     float minX = -(sprite.size.width / 2);
     
@@ -40,6 +42,54 @@
     } else {
         sprite.position = CGPointMake(sprite.position.x + acceleration, sprite.position.y);
     }
+    
+    if (acceleration < 0.0f) {
+        multiplierForDirection = 1.0f;
+    } else if (acceleration > 0.0f) {
+        multiplierForDirection = -1.0f;
+    } else {
+        multiplierForDirection = 0.0f;
+    }
+    
+    if (multiplierForDirection != 0.0f) {
+        sprite.xScale = fabs(sprite.xScale) * multiplierForDirection;
+        [self startWalking];
+    } else {
+        [self stopWalking];
+    }
+}
+
+-(void)loadWalkingSprites {
+    NSMutableArray *walkFrames = [[NSMutableArray alloc] init];
+    SKTextureAtlas *monkeyWalkingAtlas = [SKTextureAtlas atlasNamed:@"MonkeyWalking"];
+    int numberOfFrames = monkeyWalkingAtlas.textureNames.count;
+    
+    for (int i = 1; i <= numberOfFrames; i++) {
+        NSString *textureName = [NSString stringWithFormat:@"monkey-walking-%d", i];
+        SKTexture *tmp = [monkeyWalkingAtlas textureNamed:textureName];
+        [walkFrames addObject:tmp];
+    }
+
+    walkingFrames = [[NSArray alloc] init];
+    walkingFrames = walkFrames;
+}
+
+-(void)startWalking {
+    if (![sprite actionForKey:SKACTION_MONKEY_WALKING]) {
+            [sprite runAction:[SKAction repeatActionForever:[SKAction animateWithTextures:walkingFrames
+                                                                         timePerFrame:0.1f
+                                                                               resize:YES
+                                                                              restore:YES]] withKey:SKACTION_MONKEY_WALKING];
+    }
+}
+
+-(void)stopWalking {
+    [sprite removeActionForKey:SKACTION_MONKEY_WALKING];
+}
+
+-(void)wait {
+    SKTexture *texture = [SKTexture textureWithImage:[UIImage imageNamed:@"monkey"]];
+    [sprite setTexture:texture];
 }
 
 #pragma mark - Checking the item receive
@@ -51,9 +101,9 @@
             weapon = item;
         }
     } else if ([item isKindOfClass:[Malus class]]) {
-        NSLog(@"Malus");
+        
     } else if ([item isKindOfClass:[Bonus class]]) {
-        NSLog(@"Bonus");
+        
     }
 }
 
@@ -61,9 +111,6 @@
 
 -(void)launchWeapon{
     if (weapon != nil) {
-        NSLog(@"Launch weapon");
-        [[NSNotificationCenter defaultCenter] postNotificationName:NOTIFICATION_DROP_WEAPON object:nil];
-        
         [Action dropWeapon:weapon];
         weapon = nil;
     }
