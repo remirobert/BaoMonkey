b186e521de6ac558454a8c034b1c79384c3c442c
iMac0de <jeremy.peltier@epitech.eu>
2014-05-08 23:22:04 +0200

    Add home view controller

---
+++ iOS/BaoMonkey/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  BaoMonkey
+//
+//  Created by Jeremy Peltier on 08/05/2014.
+//  Copyright (c) 2014 BaoMonkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
