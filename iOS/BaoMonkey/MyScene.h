cb69b432d3385654735a9b51bf9212bac9942e00
iPPLE <ipple@iMac-EART-76.local>
2014-05-08 10:29:37 +0200

    change .h

--- iOS/BaoMonkey/MyScene.h
+++ iOS/BaoMonkey/MyScene.h
@@ -11,8 +11,13 @@
 #import "GameData.h"
 #import "GameController.h"
 #import "Monkey.h"
+#import "Item.h"
+#import "TreeBranch.h"
+#import "GameController.h"
+#import "Monkey.h"
 #import "Enemy.h"
 #import "EnemiesController.h"
+#import "ProgressBar.h"
 #import "Define.h"
 
 
