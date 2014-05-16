625dfe467fdb0d9d6e7606e11252c4fc2a6e3ad7
Remi Hillairet <remihillairet@msn.com>
2014-05-16 18:10:53 +0200

    Change enemies achievements define

--- iOS/BaoMonkey/Define.h
+++ iOS/BaoMonkey/Define.h
@@ -110,6 +110,6 @@
 
 # define    ACHIEVEMENT_POINTS  @[@[@"Achievement_500points", 500], @[@"Achievement_1000points", 1000], @[@"Achievement_1500points", 1500], @[@"Achievement_5000points", 5000]]
 # define    ACHIEVEMENT_PLUMS   @[@[@"Achievement_10plums", 10], @[@"Achievement_50plums", 50], @[@"Achievement_500plums", 500], @[@"Achievement_2500plums", 2500]]
-# define    ACHIEVEMENT_ENEMIES @[@[@"Achievement_50enemies", 50], @[@"Achievement_500enemies", 500], @[@"Achievement_5000enemies", 5000], @[@"Achievement_15000enemies", 15000]]
+# define    ACHIEVEMENT_ENEMIES @[@[@"Achievement_50Enemies", 50], @[@"Achievement_500Enemies", 500], @[@"Achievement_5000Enemies", 5000], @[@"Achievement_15000Enemies", 15000]]
 
 #endif
