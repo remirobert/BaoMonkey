32c8cf865e05fc0d2b259c855d4bd3b0065f165f
remirobert <remi.robert@epitech.eu>
2014-05-22 12:06:10 +0200

    add init Scene LamberJackMachine

---
+++ iOS/BaoMonkey/LamberJackMachineScene.m
@@ -0,0 +1,59 @@
+//
+//  LamberJackMachineScene.m
+//  BaoMonkey
+//
+//  Created by iPPLE on 22/05/2014.
+//  Copyright (c) 2014 BaoMonkey. All rights reserved.
+//
+
+#import "LamberJackMachineScene.h"
+#import "GameController.h"
+#import "TreeBranch.h"
+#import "Monkey.h"
+
+@interface LamberJackMachineScene ()
+@property (nonatomic, strong) SKScene *parentScene;
+@property (nonatomic, strong) Monkey *monkey;
+@property (nonatomic, strong) TreeBranch *treeBranch;
+@end
+
+@implementation LamberJackMachineScene
+
+- (void) initScene {
+    self.backgroundColor = [SKColor colorWithRed:52/255.0f
+                                           green:152/255.0f
+                                            blue:219/255.0f
+                                           alpha:1];
+    
+    _treeBranch = [[TreeBranch alloc] init];
+    
+    self.physicsBody = [SKPhysicsBody
+                        bodyWithEdgeLoopFromRect:CGRectMake(_treeBranch.node.frame.origin.x,
+                                                            _treeBranch.node.frame.origin.y -
+                                                            (_treeBranch.node.frame.size.height / 2) +
+                                                            (_treeBranch.node.frame.size.height / 2),
+                                                            _treeBranch.node.frame.size.width,
+                                                            _treeBranch.node.frame.size.height / 2)];
+    
+    [self addChild:_treeBranch.node];
+    
+    _monkey = [[Monkey alloc] initWithPosition:CGPointMake(self.frame.size.width/2,
+                                                           _treeBranch.node.position.y + 20)];
+    [self addChild:_monkey.sprite];
+    [GameController initAccelerometer];
+}
+
+- (instancetype) initWithSize:(CGSize)size parent:(SKScene *)parentScene {
+    self = [super initWithSize:size];
+    if (self != nil) {
+        self.view.frame = CGRectMake(0, 0, size.width, size.height);
+        self.backgroundColor = [SKColor redColor];
+
+        [self initScene];
+        
+        _parentScene = parentScene;
+    }
+    return (self);
+}
+
+@end
