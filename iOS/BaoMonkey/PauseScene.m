8dec0cf8773899293eba0a085514e45a7f3acc26
Remi Hillairet <remi.hillairet@epitech.eu>
2014-05-30 13:24:13 +0200

    Change pause assets

--- iOS/BaoMonkey/PauseScene.m
+++ iOS/BaoMonkey/PauseScene.m
@@ -19,27 +19,27 @@
         background.position = CGPointMake(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
         [self addChild:background];
         fromScene = scene;
-        panel = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"background-button"] size:CGSizeMake(306, 375)];
+        panel = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"background-panel-3buttons"] size:CGSizeMake(307, 421)];
         panel.position = CGPointMake(SCREEN_WIDTH / 2, SCREEN_HEIGHT - (panel.size.height / 2));
         [self addChild:panel];
         
-        resumeNode = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"button-play"] size:CGSizeMake(52, 52)];
-        resumeNode.position = CGPointMake(102, 380);
+        resumeNode = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"button-play"] size:CGSizeMake(80, 80)];
+        resumeNode.position = CGPointMake(163, 302);
         resumeNode.name = RESUME_NODE_NAME;
         [self addChild:resumeNode];
         
         replayNode = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"button-replay"] size:CGSizeMake(52, 52)];
-        replayNode.position = CGPointMake(224, 380);
+        replayNode.position = CGPointMake(65, 191);
         replayNode.name = RETRY_NODE_NAME;
         [self addChild:replayNode];
         
         homeNode = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"button-home"] size:CGSizeMake(52, 52)];
-        homeNode.position = CGPointMake(102, 270);
+        homeNode.position = CGPointMake(166, 191);
         homeNode.name = HOME_NODE_NAME;
         [self addChild:homeNode];
         
         settingsNode = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"button-settings"] size:CGSizeMake(52, 52)];
-        settingsNode.position = CGPointMake(224, 270);
+        settingsNode.position = CGPointMake(272, 191);
         settingsNode.name = SETTINGS_NODE_NAME;
         [self addChild:settingsNode];
     }
@@ -54,25 +54,25 @@
     
     if ([node.name isEqualToString:RESUME_NODE_NAME]) {
         [resumeNode setTexture:[SKTexture textureWithImageNamed:@"button-play-selected"]];
-        [resumeNode setSize:CGSizeMake(66, 66)];
+        [resumeNode setSize:CGSizeMake(97, 97)];
     }
     else if ([node.name isEqualToString:RETRY_NODE_NAME]) {
         [replayNode setTexture:[SKTexture textureWithImageNamed:@"button-replay-selected"]];
-        [replayNode setSize:CGSizeMake(66, 66)];
+        [replayNode setSize:CGSizeMake(61, 61)];
     }
     else if ([node.name isEqualToString:HOME_NODE_NAME]) {
         [homeNode setTexture:[SKTexture textureWithImageNamed:@"button-home-selected"]];
-        [homeNode setSize:CGSizeMake(66, 66)];
+        [homeNode setSize:CGSizeMake(61, 61)];
     }
     else if ([node.name isEqualToString:SETTINGS_NODE_NAME]) {
         [settingsNode setTexture:[SKTexture textureWithImageNamed:@"button-settings-selected"]];
-        [settingsNode setSize:CGSizeMake(66, 66)];
+        [settingsNode setSize:CGSizeMake(61, 61)];
     }
 }
 
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
     [resumeNode setTexture:[SKTexture textureWithImageNamed:@"button-play"]];
-    [resumeNode setSize:CGSizeMake(52, 52)];
+    [resumeNode setSize:CGSizeMake(80, 80)];
     [replayNode setTexture:[SKTexture textureWithImageNamed:@"button-replay"]];
     [replayNode setSize:CGSizeMake(52, 52)];
     [homeNode setTexture:[SKTexture textureWithImageNamed:@"button-home"]];
@@ -91,7 +91,7 @@
         [self.view presentScene:fromScene transition:resumeTransition];
         [(MyScene*)fromScene resumeGame];
         [resumeNode setTexture:[SKTexture textureWithImageNamed:@"button-play"]];
-        [resumeNode setSize:CGSizeMake(52, 52)];
+        [resumeNode setSize:CGSizeMake(80, 80)];
     }
     else if ([node.name isEqualToString:RETRY_NODE_NAME]) {
         [[NSNotificationCenter defaultCenter] postNotificationName:NOTIFICATION_RETRY_GAME object:nil];
