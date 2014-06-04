d551e17ee39d22f83ba5e8671792a8893fd81cd6
Yurty <brieuc.delafouchardiere@epitech.eu>
2014-06-04 14:39:26 +0200

    Update de juin
    Optimisation du css
    Mise en place du responsive en cours

--- js/app.js
+++ js/app.js
@@ -7,30 +7,31 @@ $('.social-icons a, .social-icons-bottom a').tooltip();
 	SIDE MENU
 ----------------------------------------*/
 $("#menu-close").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
-    });
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
-    });
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
 
 /*----------------------------------------
 	PAGE SCROLL
 ----------------------------------------*/
 $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
 
 /*----------------------------------------
     FLEXSLIDER
