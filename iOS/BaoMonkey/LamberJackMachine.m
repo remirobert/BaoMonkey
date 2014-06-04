44c4102e402f85da23ddea6b37f700aee8913627
remirobert <remi.robert@epitech.eu>
2014-06-04 19:18:33 +0200

    add smoke lamberJackMachine

--- iOS/BaoMonkey/LamberJackMachine.m
+++ iOS/BaoMonkey/LamberJackMachine.m
@@ -11,12 +11,15 @@
 @implementation LamberJackMachine
 
 - (void) initSprite {
-    _node = [[SKSpriteNode alloc] initWithColor:[SKColor blackColor]
-                                           size:CGSizeMake(30, 50)];
-    _node.position = CGPointMake(0, 25);
+    _node = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"machine1"]];
+    _node.size = CGSizeMake(_node.size.width / 5, _node.size.height / 5);
+    _node.position = CGPointMake(_node.size.width / 2 + 25, 25 + _node.size.height / 2);
     _node.name = LAMBER_JACK_MACHINE;
-    _node.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_node.size];
-    _node.physicsBody.affectedByGravity = NO;
+    _node.zPosition = 150;
+    [_node runAction:[SKAction repeatActionForever:[SKAction animateWithTextures:@[[SKTexture textureWithImageNamed:@"machine1"],
+                                                                                   [SKTexture textureWithImageNamed:@"machine2"],
+                                                                                   [SKTexture textureWithImageNamed:@"machine3"],
+                                                                                   [SKTexture textureWithImageNamed:@"machine4"]] timePerFrame:0.1]]];
 }
 
 - (instancetype) init {
--- iOS/BaoMonkey/LamberJackMachineScene.m
+++ iOS/BaoMonkey/LamberJackMachineScene.m
@@ -35,6 +35,18 @@
 
 @implementation LamberJackMachineScene
 
+- (void) initSmokeLamberJack {
+    NSString *burstPath =
+    [[NSBundle mainBundle] pathForResource:@"smokeLamberJack" ofType:@"sks"];
+    
+    SKEmitterNode *smoke = [NSKeyedUnarchiver unarchiveObjectWithFile:burstPath];
+    
+    smoke.zPosition = 50;
+    smoke.position = CGPointMake(_lamber.node.position.x - _lamber.node.size.width / 2,
+                                 _lamber.node.position.y);
+    [self addChild:smoke];
+}
+
 - (void) initMonkey {
     _monkey = [[Monkey alloc] initWithPosition:CGPointMake(self.frame.size.width/2, _treeBranch.node.position.y + 20)];
 
@@ -89,16 +101,18 @@
         [self initMonkey];
         
         _lamber = [[LamberJackMachine alloc] init];
+        _lamber.node.zPosition = 100;
         [self addChild:_lamber.node];
 
         self.physicsWorld.gravity = CGVectorMake(0, -10);
-
+        
         _lanchMove = NO;
         _pushforce = 2;
         _sens = rand() % 2;
         _timer = 0;
         _parentScene = parentScene;
         [self updateAngleTree];
+        [self initSmokeLamberJack];
     }
     return (self);
 }
@@ -252,9 +266,9 @@
     [self updateTreePosition];
     
     if (_lanchMove == YES) {
-        [self moveTreeBranch];
-        [self stressTree:currentTime];
-        [self toreBranch:currentTime];
+//        [self moveTreeBranch];
+//        [self stressTree:currentTime];
+//        [self toreBranch:currentTime];
     }
     
     [self enumerateChildNodesWithName:@"invalid_coco" usingBlock:^(SKNode *node, BOOL *stop) {
