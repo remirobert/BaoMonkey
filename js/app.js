01bcc59202f9bb4bcbcd4059cb6c6fb2d4cd482f
Yurty <brieuc.delafouchardiere@epitech.eu>
2014-06-11 16:59:42 +0200

    update website
    Changement d'architecture sur la page

--- js/app.js
+++ js/app.js
@@ -4,19 +4,6 @@
 $('.social-icons a, .social-icons-bottom a').tooltip();
 
 /*----------------------------------------
-	SIDE MENU
-----------------------------------------*/
-$("#menu-close").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-});
-
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-});
-
-/*----------------------------------------
 	PAGE SCROLL
 ----------------------------------------*/
 $('a[href*=#]:not([href=#])').click(function() {
@@ -34,15 +21,48 @@ $('a[href*=#]:not([href=#])').click(function() {
 });
 
 /*----------------------------------------
-    FLEXSLIDER
+    PAGE SCROLL
 ----------------------------------------*/
-$(window).load(function(){
-  $('.flexslider').flexslider({
-    animation: "slide",
-    directionNav: false,
-    start: function(slider){
-      $('body').removeClass('loading');
-    }
-  });
-});
+function redimensionnement(){ 
+ 
+    var $image = $('img.superbg');
+    var image_width = $image.width(); 
+    var image_height = $image.height();     
+     
+    var over = image_width / image_height; 
+    var under = image_height / image_width; 
+     
+    var body_width = $(window).width(); 
+    var body_height = $(window).height(); 
+     
+    if (body_width / body_height >= over) { 
+      $image.css({ 
+        'width': body_width + 'px', 
+        'height': Math.ceil(under * body_width) + 'px', 
+        'left': '0px', 
+        'top': Math.abs((under * body_width) - body_height) / -2 + 'px' 
+      }); 
+    }  
+     
+    else { 
+      $image.css({ 
+        'width': Math.ceil(over * body_height) + 'px', 
+        'height': body_height + 'px', 
+        'top': '0px', 
+        'left': Math.abs((over * body_height) - body_width) / -2 + 'px' 
+      }); 
+    } 
+} 
+      
+$(document).ready(function(){
+
+    // Au chargement initial   
+    redimensionnement();
+     
+    // En cas de redimensionnement de la fenêtre
+    $(window).resize(function(){ 
+        redimensionnement(); 
+    }); 
+ 
+}); 
 
