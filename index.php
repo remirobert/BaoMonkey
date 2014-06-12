0e2ec090ccb9abbbfffbe8e4760fc7bd14b78fb0
Yurty <brieuc.delafouchardiere@epitech.eu>
2014-06-12 17:43:23 +0200

    Add footer and update bg test

--- index.php
+++ index.php
@@ -12,6 +12,22 @@
                 <div class="col-md-12">
                     <img class="logo" src="assets/logo.png" alt="logo">
                 </div>
+                <footer class="footer">
+                    <div class="container">    
+                        <div class="row">
+                          <div class="col-md-6">
+                            <p class="copyright">Copyright 2014 - Baomonkey - <a href="#">Terms of use</a></p>
+                          </div>
+                          <div class="col-md-6">
+                            <ul class="list-inline">
+                                <li><a href="contact.php">Contact</a></li>
+                                <li><a href="press.php">PressKit</a></li>
+                                <li><a href="team.php">Team</a></li>
+                            </ul>  
+                          </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
         </div>
     </section>
