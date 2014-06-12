66fa7f6f25082fa44e0f67d7c0e92d45e02ada1b
Yurty <brieuc.delafouchardiere@epitech.eu>
2014-06-12 11:43:10 +0200

    add includes header and footer.php

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<!--
+	BAOMONKEY 2014
+	FOOTER
+-->
+
+		<!-- JavaScripts -->
+		<script src="js/jquery-1.10.2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/app.js"></script>
+        
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+    include 'includes/header.php';
+
+?>
+            
+    <!-- MAIN SECTION
+	============================== -->
+    <section id="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <img class="logo" src="assets/logo.png" alt="logo">
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php
+
+    include 'includes/footer.php';
+
+?>
