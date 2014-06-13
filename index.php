ad9466048f7c3f4bf47980e0162f6ca20abc7b2a
Yurty <brieuc.delafouchardiere@epitech.eu>
2014-06-13 15:04:04 +0200

    Update responsive

--- index.php
+++ index.php
@@ -4,33 +4,29 @@
 
 ?>
             
-    <!-- MAIN SECTION
+    <!-- Main page
 	============================== -->
-    <section id="hero">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
+    <div class="site-wrapper">
+        <div class="site-wrapper-inner">
+            <div class="cover-container">
+
+                <div class="inner cover">
                     <img class="logo" src="assets/logo.png" alt="logo">
                 </div>
-                <footer class="footer">
-                    <div class="container">    
-                        <div class="row">
-                          <div class="col-md-6">
-                            <p class="copyright">Copyright 2014 - Baomonkey - <a href="#">Terms of use</a></p>
-                          </div>
-                          <div class="col-md-6">
-                            <ul class="list-inline">
-                                <li><a href="contact.php">Contact</a></li>
-                                <li><a href="press.php">PressKit</a></li>
-                                <li><a href="team.php">Team</a></li>
-                            </ul>  
-                          </div>
-                        </div>
-                    </div>
-                </footer>
+
+                <div class="mastfoot">
+                    <ul class="list-inline">
+                        <li><a href="team.php">Team</a></li>
+                        <li><a href="press.php">PressKit</a></li>
+                        <li><a href="terms.php">Terms of use</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+        
             </div>
         </div>
-    </section>
+    </div>
+
 
 <?php
 
