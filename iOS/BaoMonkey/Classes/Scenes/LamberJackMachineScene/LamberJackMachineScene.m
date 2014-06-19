93a34a6bf1b2ed3ff4676a2f9e25547e8c99008e
remirobert <remi.robert@epitech.eu>
2014-06-19 15:10:00 +0200

    fix design lamberJack Machine geante

--- iOS/BaoMonkey/Classes/Scenes/LamberJackMachineScene/LamberJackMachineScene.m
+++ iOS/BaoMonkey/Classes/Scenes/LamberJackMachineScene/LamberJackMachineScene.m
@@ -29,11 +29,55 @@
 @property (nonatomic, strong) LamberJackMachine *lamber;
 @property (nonatomic, assign) NSInteger sens;
 @property (nonatomic, assign) BOOL lanchMove;
-@property (nonatomic, strong) SKSpriteNode *tree;
+@property (nonatomic, strong) SKSpriteNode *bg;
+@property (nonatomic, strong) SKSpriteNode *trunk;
+@property (nonatomic, strong) SKSpriteNode *backLeaf;
 @property (nonatomic, strong) SKSpriteNode *frontLeaf;
 @end
 
 @implementation LamberJackMachineScene
+
+- (void) initTrunk {
+    _trunk = [SKSpriteNode spriteNodeWithImageNamed:@"trunk-step-1"];
+    _trunk.position = [BaoPosition trunk];
+    _trunk.name = TRUNK_NODE_NAME;
+    
+    NSInteger trunkLife = [GameData getTrunkLife];
+    static NSInteger step = 0;
+    
+    if ((trunkLife > 90 && trunkLife <= 100) && step != 0) {
+        [_trunk setTexture:[SKTexture textureWithImageNamed:@"trunk-step-0"]];
+        step = 0;
+    } else if ((trunkLife > 80 && trunkLife <= 90) && step != 1) {
+        [_trunk setTexture:[SKTexture textureWithImageNamed:@"trunk-step-1"]];
+        step = 1;
+    } else if ((trunkLife > 70 && trunkLife <= 80) && step != 2) {
+        [_trunk setTexture:[SKTexture textureWithImageNamed:@"trunk-step-2"]];
+        step = 2;
+    } else if ((trunkLife > 60 && trunkLife <= 70) && step != 3) {
+        [_trunk setTexture:[SKTexture textureWithImageNamed:@"trunk-step-3"]];
+        step = 3;
+    } else if ((trunkLife > 50 && trunkLife <= 60) && step != 4){
+        [_trunk setTexture:[SKTexture textureWithImageNamed:@"trunk-step-4"]];
+        step = 4;
+    } else if ((trunkLife > 40 && trunkLife <= 50) && step != 5) {
+        [_trunk setTexture:[SKTexture textureWithImageNamed:@"trunk-step-5"]];
+        step = 5;
+    } else if ((trunkLife > 30 && trunkLife <= 40) && step != 6) {
+        [_trunk setTexture:[SKTexture textureWithImageNamed:@"trunk-step-6"]];
+        step = 6;
+    } else if ((trunkLife > 20 && trunkLife <= 30) && step != 7) {
+        [_trunk setTexture:[SKTexture textureWithImageNamed:@"trunk-step-7"]];
+        step = 7;
+    } else if ((trunkLife > 10 && trunkLife <= 20) && step != 8) {
+        [_trunk setTexture:[SKTexture textureWithImageNamed:@"trunk-step-8"]];
+        step = 8;
+    } else if ((trunkLife > 0 && trunkLife <= 10) && step != 9){
+        [_trunk setTexture:[SKTexture textureWithImageNamed:@"trunk-step-9"]];
+        step = 9;
+    }
+    [self addChild:_trunk];
+}
 
 - (void) initSmokeLamberJack {
     NSString *burstPath =
@@ -47,25 +91,50 @@
     [self addChild:smoke];
 }
 
+- (void) initMonkeyPhysic {
+    CGFloat offsetX = _monkey.sprite.frame.size.width * _monkey.sprite.anchorPoint.x;
+    CGFloat offsetY = _monkey.sprite.frame.size.height * _monkey.sprite.anchorPoint.y;
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    
+    CGPathMoveToPoint(path, NULL, 48 - offsetX, 46 - offsetY);
+    CGPathAddLineToPoint(path, NULL, 17 - offsetX, 45 - offsetY);
+    CGPathAddLineToPoint(path, NULL, 9 - offsetX, 28 - offsetY);
+    CGPathAddLineToPoint(path, NULL, 13 - offsetX, 6 - offsetY);
+    CGPathAddLineToPoint(path, NULL, 13 - offsetX, 6 - offsetY);
+    CGPathAddLineToPoint(path, NULL, 22 - offsetX, 1 - offsetY);
+    CGPathAddLineToPoint(path, NULL, 46 - offsetX, 2 - offsetY);
+    CGPathAddLineToPoint(path, NULL, 55 - offsetX, 17 - offsetY);
+    
+    CGPathCloseSubpath(path);
+    
+    _monkey.sprite.physicsBody = [SKPhysicsBody bodyWithPolygonFromPath:path];
+}
+
 - (void) initMonkey {
     _monkey = [[Monkey alloc] initWithPosition:CGPointMake(self.frame.size.width/2, [UIScreen mainScreen].bounds.size.height)];
+
+    [self initMonkeyPhysic];
     
-    _monkey.sprite.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(_monkey.collisionMask.size.width,
-                                                                                   _monkey.collisionMask.size.height)];
     _monkey.sprite.physicsBody.affectedByGravity = YES;
     _monkey.sprite.physicsBody.mass = 10;
     _monkey.sprite.physicsBody.allowsRotation = NO;
+    _monkey.sprite.physicsBody.usesPreciseCollisionDetection = YES;
     _monkey.sprite.name = @"monkey_node_name";
-    
     [self addChild:_monkey.sprite];
 }
 
 - (void) initScene {
-    _tree = [SKSpriteNode spriteNodeWithImageNamed:@"background-center"];
-    _tree.position = CGPointMake((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2));
-    [self addChild:_tree];
+    _bg = [SKSpriteNode spriteNodeWithImageNamed:@"background-center"];
+    _bg.position = CGPointMake((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2));
+    [self addChild:_bg];
+
     
-    
+    _backLeaf = [SKSpriteNode spriteNodeWithImageNamed:@"back-leafs"];
+    _backLeaf.position = [BaoPosition backLeafs:_backLeaf.size];
+    _backLeaf.name = BACK_LEAF_NODE_NAME;
+    [self addChild:_backLeaf];
+
     _frontLeaf = [SKSpriteNode spriteNodeWithImageNamed:@"leafs"];
 //    _frontLeaf.size = CGSizeMake(_frontLeaf.size.width / 3, _frontLeaf.size.height / 3);
     _frontLeaf.position = CGPointMake((SCREEN_WIDTH / 2), (SCREEN_HEIGHT - (_frontLeaf.size.height / 2)));
@@ -88,6 +157,7 @@
     //_tree.size = CGSizeMake(_tree.size.width / 2, _tree.size.height / 2);
     
     [self addChild:_treeBranch.node];
+    [self initTrunk];
     
     _monkey.sprite.position = CGPointMake([UIScreen mainScreen].bounds.size.width / 2,
                                           _treeBranch.node.position.y + (_treeBranch.node.size.height / 2));
@@ -136,7 +206,8 @@
         [coco.node runAction:[SKAction waitForDuration:(float)arc4random() / 0x100000000] completion:^{
             NSLog(@"physic");
             coco.node.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:coco.node.size];
-            coco.node.physicsBody.mass = 40.0;
+            coco.node.physicsBody.mass = 25.0;
+            coco.node.physicsBody.usesPreciseCollisionDetection = YES;
         }];
     }
 }
@@ -213,7 +284,8 @@
 }
 
 - (void) updateTreePosition {
-    _tree.zRotation = _treeBranch.node.zRotation;
+    _backLeaf.zRotation = _treeBranch.node.zRotation;
+    _trunk.zRotation = _treeBranch.node.zRotation;
     _frontLeaf.zRotation = _treeBranch.node.zRotation;
 }
 
@@ -271,7 +343,7 @@
     if (_lanchMove == YES) {
         [self moveTreeBranch];
         [self stressTree:currentTime];
-        //[self toreBranch:currentTime];
+        [self toreBranch:currentTime];
     }
     
     [self enumerateChildNodesWithName:@"invalid_coco" usingBlock:^(SKNode *node, BOOL *stop) {
--- iOS/BaoMonkey/Classes/Scenes/MyScene/MyScene.m
+++ iOS/BaoMonkey/Classes/Scenes/MyScene/MyScene.m
@@ -313,7 +313,13 @@
 -(void)update:(CFTimeInterval)currentTime {
 
     NSInteger oldLevel = [GameData getLevel];        
-        
+
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self loadLamberJackGeantMachineScene];
+    });
+    
+    
     if ([[GameData singleton] isPause]) {
         
         dispatch_once(&oncePause, ^{
