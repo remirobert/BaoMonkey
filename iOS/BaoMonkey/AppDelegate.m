0ac102d8d60627e4984be2537196d4f3c4f26ad1
Remi Hillairet <remi.hillairet@epitech.eu>
2014-06-29 18:02:48 +0200

    Pause music when app is not active + Preload commando-walking-dead with correct filenames

--- iOS/BaoMonkey/AppDelegate.m
+++ iOS/BaoMonkey/AppDelegate.m
@@ -33,9 +33,8 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    
+    [Music pauseBackgroundMusic];
     if ([GameData isPause] == NO) {
-        [Music pauseBackgroundMusic];
         [[NSNotificationCenter defaultCenter] postNotificationName:NOTIFICATION_PAUSE_GAME object:nil];
         [UserData saveUserData];
     }
